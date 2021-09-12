7e97476aa30ca63334accbabdd8db5325c94ec77
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-09-12 17:15:41 +0500

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import REACT_LOGO from '@assets/react.png';
 import './style.css';
 
 const App = () => {
+	const sampleObject = {
+		prop1:{
+			prop2:'test'
+		}
+	}
     return (
 			<div>
 				<img src={REACT_LOGO} height="200px" />
         <h1>
-            Hello World
+            Hello World {sampleObject?.prop1?.prop2}
         </h1>
 			</div>
     );
